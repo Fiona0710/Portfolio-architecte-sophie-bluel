1dcf7672b4eb995b4b07d8765016316aaf538bda
Fiona0710 <berthou.fiona@hotmail.fr>
2023-05-11 14:48:59 +0200

    POST request to the API for admin connection, if valid registration of the token in localStorage , messages displayed in the console and redirection to the home page

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,16 +1,42 @@
+
 const form = document.getElementById('form');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
 
+const url = 'http://localhost:5678/api/users/login';
 
-form.addEventListener('submit',(event)=>{
-  event.preventDefault(); //Empeche l'envoie du formulaire 
-    
-    fetch('http://localhost:5678/api/users/login', {
+//évenement au moment de soumettre le formulaire 
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  //try et catch servent a afficher les erreurs potentielles dans la console.
+  try {
+    console.log(email);
+    const response = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ email:"sophie.bluel@test.tld", password: "S0phie" }) 
+      body: JSON.stringify({
+        email: email.value ,
+        password: password.value})
     });
+
+    const responseData = await response.json();
+
+    console.log(responseData);
+
+   if (response.ok) {
+      const token = responseData.token;
+      window.localStorage.setItem("token", token);
+      // window.location.href = './index.html';
+      console.log(token);
+      console.log("Vous êtes connecté :-)");
+    } else {
+      console.log("L'email ou le mot de passe n'est pas valide");
+    } 
+  } catch (error) {
+    console.error(error);
+  }
 });
+
+
+
