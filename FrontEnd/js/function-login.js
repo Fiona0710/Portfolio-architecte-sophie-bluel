e5b03fd5443fb5771766a876ea50ea4eda0a89c5
Fiona0710 <berthou.fiona@hotmail.fr>
2023-05-12 16:04:16 +0200

    Adding the error message, splitting the code with functions

---
+++ FrontEnd/js/function-login.js
@@ -0,0 +1,54 @@
+
+  // Fonction pour afficher les messages d'erreur
+function showError(message) {
+    const errorSpan = document.getElementById('erreur');
+    errorSpan.innerHTML = message;
+    errorSpan.style.color = "#B1663C";
+    errorSpan.style.fontWeight = "bold";
+    errorSpan.style.fontFamily = "'Syne', sans-serif";
+    errorSpan.style.fontSize = "1.2em";
+  }
+  
+  // Fonction pour soumettre le formulaire
+async function submitForm(event) {
+    event.preventDefault();
+
+  // try et catch servent à afficher les erreurs potentielles dans la console.
+    try {
+    //Requete POST pour envoyer les données sur l'api     
+      const url = 'http://localhost:5678/api/users/login';
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email.value,
+          password: password.value
+        })
+      });
+  
+      const responseData = await response.json();
+
+    // Vérification du code de statut de la réponse du serveur
+      if (response.status === 200) {
+        const token = responseData.token;
+        //Enregistrement du token et redirection vers la page d'acceuil
+        window.localStorage.setItem("token", token);
+        window.location.href = './index.html';
+
+    //Affichage de differents messages d'erreur en fonction du code recu
+      } else if (response.status === 401) {
+        showError("L'email ou et le mot de passe n'est pas valide.<br>La connexion n'est pas autorisée !!!");
+
+      } else if (response.status === 404) {
+        showError("L'utilisateur ne se trouve pas dans la base de données.");
+      }
+      
+    //Affichage d'un message d'erreur si aucune connexion au serveur   
+    } catch (error) {
+      console.error(error);
+      showError("Une erreur est survenue lors de la connexion.<br>Veuillez réessayer plus tard.");
+    }
+};
+  
--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,42 +1,4 @@
-
 const form = document.getElementById('form');
-
-const url = 'http://localhost:5678/api/users/login';
-
-//évenement au moment de soumettre le formulaire 
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-
-  //try et catch servent a afficher les erreurs potentielles dans la console.
-  try {
-    console.log(email);
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        email: email.value ,
-        password: password.value})
-    });
-
-    const responseData = await response.json();
-
-    console.log(responseData);
-
-   if (response.ok) {
-      const token = responseData.token;
-      window.localStorage.setItem("token", token);
-      // window.location.href = './index.html';
-      console.log(token);
-      console.log("Vous êtes connecté :-)");
-    } else {
-      console.log("L'email ou le mot de passe n'est pas valide");
-    } 
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-
+// Evénement au moment de soumettre le formulaire 
+form.addEventListener('submit', submitForm);
 
