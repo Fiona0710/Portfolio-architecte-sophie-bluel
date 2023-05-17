c6cc41916642f506653a4a99af7a9878a3f03ded
Fiona0710 <berthou.fiona@hotmail.fr>
2023-05-17 11:26:13 +0200

    Added 'header-login' class in Javascript

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,3 +1,5 @@
+const header =document.querySelector("header");
+header.classList.add("header-logout"); 
 const form = document.getElementById('form');
 // Evénement au moment de soumettre le formulaire 
 form.addEventListener('submit', submitForm);
