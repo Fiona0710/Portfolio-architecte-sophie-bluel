f1c86eccbadf8b06384257278374567850fa1b16
Fiona0710 <berthou.fiona@hotmail.fr>
2023-06-06 17:31:09 +0200

    final touch

--- FrontEnd/js/admin.js
+++ FrontEnd/js/admin.js
@@ -1,10 +1,44 @@
+/*POSTING AND THE ADMINISTRATOR SIDE*/
 
 const displayLogout = document.getElementById("logout");
+
+// when clicking on logout the user disconnects
+displayLogout.addEventListener("click",() =>{
+  window.localStorage.removeItem("token");
+
+  // redirect to offline homepage
+  window.location.href = "./index.html";
+});
+
+// Recovery of the token in the local storage
 const token = window.localStorage.getItem("token");
 
-/************************* Fonctions pour rajouter des élèments coté admin ***********************/
+// if the user closes the tab or quits the browser, he logs out 
+window.addEventListener('unload', () => {
+  window.localStorage.removeItem("token");
+});
 
-// fonction pour creer le bouton et l'icon "modifier"
+// Display of elements on the administrator side if token present
+if (token) {
+    displayLogout.textContent = "logout";
+  
+    const filters = document.getElementById("filters");
+    setDisplayStyle(filters, 'none');
+
+    const editImg = document.querySelector("#introduction figure");
+          editImg.appendChild(createEditElement());
+
+    const editArticle = document.querySelector("#introduction article");
+          editArticle.insertBefore(createEditElement(), editArticle.firstChild);
+  
+    const directionEdit = document.getElementById("directionEdit");
+          directionEdit.appendChild(createEditElement()); 
+    displayHeadbandEditMod();
+}
+
+/************************************* Functions *****************************************/
+
+// function to create the "modify" button and icon
 function createEditElement() {
   const displayEdit = createButtonElement(['positionEdit'],"modifier");
  
@@ -14,7 +48,7 @@ function createEditElement() {
   return displayEdit;
 };
 
-// Fonction pour afficher la bande noir "mode édition"
+// Function to display the "edit mode" black band
 function displayHeadbandEditMod(){
   const header = document.querySelector("header");
   header.style.marginTop = "100px";
@@ -35,35 +69,6 @@ function displayHeadbandEditMod(){
 };
 
 /**********************************************************************************************/
-  
-if (token) {
-    displayLogout.textContent = "logout";
-  
-    const filters = document.getElementById("filters");
-    setDisplayStyle(filters, 'none');
-
-    const editImg = document.querySelector("#introduction figure");
-          editImg.appendChild(createEditElement());
-
-    const editArticle = document.querySelector("#introduction article");
-          editArticle.insertBefore(createEditElement(), editArticle.firstChild);
-  
-    const directionEdit = document.getElementById("directionEdit");
-          directionEdit.appendChild(createEditElement()); 
-    displayHeadbandEditMod();
-}
-// si l'utilisateur ferme l'onglet ou quitte le navigateur ,il se deconnecte   
-window.addEventListener('unload', () => {
-  window.localStorage.removeItem("token");
-});
-
-// au click sur logout l'utilisateur se deconnecte
-displayLogout.addEventListener("click",() =>{
-  window.localStorage.removeItem("token");
-  // redirection vers la page de d'acceuil non connectée
-  window.location.href = "./index.html";
-}); 
-
 
 
 
